fa27d63f1e40d7e645a34a77676cdb4f84b4cf37
Pushpraj Patil <patilpushpraj39@gmail.com>
2026-09-05 14:51:44 +0530

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,9 @@
-import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
-
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
   const router = createRouter({
     routeTree,
+    basepath: "/cafe-d-cruze-v2",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
